a0ebe964eecd4fc6cb94a7fca0780e9ac2f67b34
Origguel <arthur.mdb.mozaicstudio@gmail.com>
2025-05-13 12:24:08 +0200

    [FEAT] home icon alignement

--- app/components/home/home_grid.py
+++ app/components/home/home_grid.py
@@ -20,37 +20,6 @@ class HomeGrid(QWidget):
     def __init__(self):
         super().__init__()
 
-        def create_background_cell(image_path: str, object_name: str = "home_cell") -> QFrame:
-            frame = QFrame()
-            frame.setObjectName(object_name)
-            frame.setStyleSheet(f"""
-                QFrame#{object_name} {{
-                    border: 1px solid #2B2B2B;
-                    border-radius: 6px;
-                    background-color: transparent;
-                }}
-            """)
-
-            label = QLabel(frame)
-            label.setScaledContents(True)  # ← important pour forcer le fill sans bug
-            pixmap = QPixmap(image_path)
-            label.setPixmap(pixmap.scaled(
-                frame.size(), Qt.KeepAspectRatioByExpanding, Qt.SmoothTransformation
-            ))
-            label.setGeometry(0, 0, frame.width(), frame.height())
-            label.lower()
-
-            def resize_background():
-                label.setPixmap(pixmap.scaled(
-                    frame.size(), Qt.KeepAspectRatioByExpanding, Qt.SmoothTransformation
-                ))
-                label.setGeometry(0, 0, frame.width(), frame.height())
-
-            frame.resizeEvent = lambda event: resize_background()
-
-            return frame
-
-
         # Créer un layout de type GridLayout
         self.home_layout = QGridLayout(self)
         self.home_layout.setSpacing(12)  # Espacement de 12px entre les cellules
@@ -65,7 +34,7 @@ class HomeGrid(QWidget):
 
         # Fusionner les premières 2 lignes x 6 colonnes en une seule cellule
         cell_projects = QWidget()
-        cell_projects.setObjectName("Home_Box")
+        cell_projects.setObjectName("home_cell")
         self.home_layout.addWidget(cell_projects, 0, 0, 2, 6)  # Fusion de 2 lignes x 6 colonnes (positions (0, 0))
 
         # Ajouter une cellule 2x1 après la 2x6
@@ -75,37 +44,42 @@ class HomeGrid(QWidget):
         button_notes = ButtonIcon(icon_name="arrow_big_Right", style="Button_Secondary_Icon", parent=cell_notes)
         button_notes.clicked.connect(self.on_button_notes_click)
         cell_notes_layout.addWidget(button_notes)
-        cell_notes_layout.setAlignment(Qt.AlignCenter)
+        cell_notes_layout.setAlignment(Qt.AlignBottom | Qt.AlignRight)
         self.home_layout.addWidget(cell_notes, 0, 6, 2, 1)
 
-        # ...
-        cell_solarpunk = create_background_cell("assets/img/home_background/solarpunk.png")
-        self.home_layout.addWidget(cell_solarpunk, 0, 7, 1, 2)
+        cell_solarpunk = QWidget()
+        cell_solarpunk.setObjectName("cell_solarpunk")
+        cell_solarpunk_layout = QVBoxLayout(cell_solarpunk)
+        button_solarpunk = ButtonIcon(icon_name="arrow_big_Right", style="Button_Secondary_Icon", parent=cell_solarpunk)
+        button_solarpunk.clicked.connect(self.on_button_solarpunk_click)
+        cell_solarpunk_layout.addWidget(button_solarpunk)
+        cell_solarpunk_layout.setAlignment(Qt.AlignBottom | Qt.AlignRight)
+        self.home_layout.addWidget(cell_solarpunk, 0, 7, 1, 2)  # Positionner la cellule à (0, 6) avec 2 lignes et 1 colonne
 
         # Gestion de Projets (utilise ButtonText)
         cell_gestion = QWidget()
-        cell_gestion.setObjectName("Home_Box")
+        cell_gestion.setObjectName("home_cell")
         cell_gestion_layout = QVBoxLayout(cell_gestion)
         button_gestion = ButtonIcon(icon_name="arrow_big_Right", style="Button_Secondary_Icon", parent=cell_gestion)
         button_gestion.clicked.connect(self.on_button_gestion_click)
         cell_gestion_layout.addWidget(button_gestion)
-        cell_gestion_layout.setAlignment(Qt.AlignCenter)
+        cell_gestion_layout.setAlignment(Qt.AlignBottom | Qt.AlignRight)
         self.home_layout.addWidget(cell_gestion, 1, 7, 1, 1)
 
         # Statistiques (utilise ButtonText)
         cell_stat = QWidget()
-        cell_stat.setObjectName("Home_Box")
+        cell_stat.setObjectName("home_cell")
         cell_stat_layout = QVBoxLayout(cell_stat)
         button_stat = ButtonIcon(icon_name="arrow_big_Right", style="Button_Secondary_Icon", parent=cell_stat)
         button_stat.clicked.connect(self.on_button_stat_click)
         cell_stat_layout.addWidget(button_stat)
-        cell_stat_layout.setAlignment(Qt.AlignCenter)
+        cell_stat_layout.setAlignment(Qt.AlignBottom | Qt.AlignRight)
         self.home_layout.addWidget(cell_stat, 1, 8, 1, 1)
 
 
         # Ajouter d'autres cellules
         cell_calendrier = QWidget()
-        cell_calendrier.setObjectName("Home_Box")
+        cell_calendrier.setObjectName("home_cell")
         self.home_layout.addWidget(cell_calendrier, 2, 0, 3, 9)  # Positionner la cellule à (0, 6) avec 2 lignes et 1 colonne
 
         # Applique le layout au widget parent
@@ -125,6 +99,9 @@ class HomeGrid(QWidget):
         """Méthode appelée lorsqu'on clique sur le bouton 'Statistiques'"""
         self.switch_to_stats_page.emit()  # Déclenche l'événement pour changer de page
         self.change_dropdown_to_stats()
+
+    def on_button_solarpunk_click(self):
+        print("Page Solarpunk")
 
     def change_dropdown_to_notes(self):
         """Change l'index de la dropdown pour afficher la page des Notes"""

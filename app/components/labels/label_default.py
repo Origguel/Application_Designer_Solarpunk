e761fe1f92e3e2955d8561c90b9244c2a4b5a625
Origguel <arthur.mdb.mozaicstudio@gmail.com>
2025-05-18 23:02:49 +0200

    [UI] note detail widget design

--- app/components/labels/label_default.py
+++ app/components/labels/label_default.py
@@ -1,9 +1,17 @@
-from PySide6.QtWidgets import QLabel
+from PySide6.QtWidgets import QLabel, QSizePolicy
+from PySide6.QtCore import Qt
 
 class LabelDefault(QLabel):
-    def __init__(self ,text="Texte du bouton", style="Label_Default", x=376, y=32, parent=None):
+    def __init__(self ,text="Texte du bouton", style="Label_Default", x=376, y=None, parent=None):
         super().__init__(parent)
         self.setText(text)
         self.setObjectName(style)
-        self.setFixedHeight(y)
+        self.setWordWrap(True)
+        self.setAlignment(Qt.AlignTop | Qt.AlignLeft)
+
         self.setFixedWidth(x)
+
+        if y is not None:
+            self.setFixedHeight(y)
+        else:
+            self.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Minimum)

dce6f49801f59d8207f8eacadf988b4b73f48c9e
Origguel <arthur.mdb.mozaicstudio@gmail.com>
2025-05-27 01:07:28 +0200

    [ANIM] Better animation timing

--- app/utils/animations/project_ui_animation.py
+++ app/utils/animations/project_ui_animation.py
@@ -1,7 +1,7 @@
 from PySide6.QtCore import QEasingCurve, QPoint, QPropertyAnimation, QPauseAnimation, QSequentialAnimationGroup
 
 
-def get_leave_animation(widget, screen_width, duration=250, delay=0, overshoot=0.6):
+def get_leave_animation(widget, screen_width, duration=300, delay=0, overshoot=0.6):
     original_pos = widget.pos()
     offscreen_right = QPoint(screen_width + 100, original_pos.y())
 
@@ -24,7 +24,7 @@ def get_leave_animation(widget, screen_width, duration=250, delay=0, overshoot=0
 
 
 
-def get_enter_animation(widget, screen_width, duration=750, delay=0, overshoot=0.6):
+def get_enter_animation(widget, screen_width, duration=300, delay=0, overshoot=0.6):
     original_pos = widget.pos()
     offscreen_right = QPoint(screen_width + 100, original_pos.y())
 
@@ -49,8 +49,8 @@ def get_enter_animation(widget, screen_width, duration=750, delay=0, overshoot=0
 
 
 def play_enter_exit_sequence(widget, screen_width, on_mid_callback=None, duration=1000):
-    leave = get_leave_animation(widget, screen_width, duration=250, delay=0, overshoot=0.6)
-    enter = get_enter_animation(widget, screen_width, duration=750, delay=500, overshoot=0.6)
+    leave = get_leave_animation(widget, screen_width, duration=300, delay=0, overshoot=0.6)
+    enter = get_enter_animation(widget, screen_width, duration=300, delay=100, overshoot=0.6)
 
     sequence = QSequentialAnimationGroup(widget)  # 👈 optionnel mais bon pour attacher
     sequence.addAnimation(leave)
